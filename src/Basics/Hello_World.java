b5ab6636c9a805f01766b5a3f0f5863a6e7005c8
Phani Bhushan S <phani.bhushan@Aurigo.com>
2021-11-30 19:47:49 +0530

    Basic Folder STructure and Examples

---
+++ src/Basics/Hello_World.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class Hello_World {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        hello();
+        int x = 10;
+        System.out.println(x);
+        x = 20;
+        System.out.println(x);
+    }
+    public static void hello()
+    {
+        System.out.println("Hello Java");
+    }
+}
